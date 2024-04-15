beb66bbaf2f5713fc33c5795b33fe354f988b480
TWTom <57289975+TWTom041@users.noreply.github.com>
2024-04-15 21:23:21 +0800

    Update gform.js

--- assets/gform.js
+++ assets/gform.js
@@ -1,15 +1,20 @@
 const ALL_ENT = ["entry.117793984","entry.43731313","entry.1206758089","entry.179139450","entry.2110442727","entry.381768359","entry.730313804","entry.1958654718","entry.271334770","entry.1993577002","entry.869053444","entry.1852148494"]
 const formToObject = form => Object.fromEntries(new FormData(form));
 
-$('#bootstrapForm').submit(function (event) {
-    event.preventDefault()
-    var extraData = {}
+function calc_score() {
     let score = 0;
     let form_dat = formToObject(document.querySelector('#bootstrapForm')); 
     ALL_ENT.forEach((ent) => {
       score += Number(form_dat[ent]);
     })
-    alert(score);
+    return score;
+}
+
+
+$('#bootstrapForm').submit(function (event) {
+    event.preventDefault()
+    var extraData = {}
+    
     // Submit of form should be successful but JSONP callback will fail because Google Forms
     // does not support it, so this is handled as a failure.
     alert('Form Submitted. Thanks.');
