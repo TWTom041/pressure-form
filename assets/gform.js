bc8aa8e7ff0265cd4da2050cfad7f2971fc0f7eb
TWTom <57289975+TWTom041@users.noreply.github.com>
2024-04-15 21:06:37 +0800

    Update gform.js

--- assets/gform.js
+++ assets/gform.js
@@ -4,19 +4,20 @@ const formToObject = form => Object.fromEntries(new FormData(form));
 $('#bootstrapForm').submit(function (event) {
     event.preventDefault()
     var extraData = {}
+    let score = 0;
+    let form_dat = formToObject(document.querySelector('#bootstrapForm')); 
+    ALL_ENT.forEach((ent) => {
+      score += Number(form_dat[ent]);
+    })
+    alert(score);
+    // Submit of form should be successful but JSONP callback will fail because Google Forms
+    // does not support it, so this is handled as a failure.
+    alert('Form Submitted. Thanks.');
     $('#bootstrapForm').ajaxSubmit({
         data: extraData,
         dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
         error: function () {
-            let score = 0;
-            let form_dat = formToObject(document.querySelector('#bootstrapForm')); 
-            ALL_ENT.forEach((ent) => {
-              score += Number(form_dat[ent]);
-            })
-            alert(score);
-            // Submit of form should be successful but JSONP callback will fail because Google Forms
-            // does not support it, so this is handled as a failure.
-            alert('Form Submitted. Thanks.');
+            
             // You can also redirect the user to a custom thank-you page:
             // window.location = 'http://www.mydomain.com/thankyoupage.html'
         }
