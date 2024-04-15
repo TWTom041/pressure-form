85bbd8e42a1dab8d7ab5d516a7ab35e4b588a062
TWTom <57289975+TWTom041@users.noreply.github.com>
2024-04-15 21:54:21 +0800

    Update gform.js

--- assets/gform.js
+++ assets/gform.js
@@ -11,20 +11,22 @@ function calc_score() {
 }
 
 
-$('#bootstrapForm').submit(function (event) {
-    event.preventDefault()
-    var extraData = {}
-    
-    // Submit of form should be successful but JSONP callback will fail because Google Forms
-    // does not support it, so this is handled as a failure.
-    alert('Form Submitted. Thanks.');
-    $('#bootstrapForm').ajaxSubmit({
-        data: extraData,
-        dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
-        error: function () {
-            
-            // You can also redirect the user to a custom thank-you page:
-            // window.location = 'http://www.mydomain.com/thankyoupage.html'
-        }
+$(document).ready(function() {
+    $('#bootstrapForm').submit(function (event) {
+        event.preventDefault()
+        var extraData = {}
+        
+        // Submit of form should be successful but JSONP callback will fail because Google Forms
+        // does not support it, so this is handled as a failure.
+        alert('Form Submitted. Thanks.');
+        $('#bootstrapForm').ajaxSubmit({
+            data: extraData,
+            dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
+            error: function () {
+                
+                // You can also redirect the user to a custom thank-you page:
+                // window.location = 'http://www.mydomain.com/thankyoupage.html'
+            }
+        })
     })
-})
+}
